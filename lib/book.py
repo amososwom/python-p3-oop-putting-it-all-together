8ef06b6cafe2e8854f3d57ad9def99b3b801df8f
primeboogie <primemarkboogie@gmail.com>
2024-05-30 17:57:22 -0100

    completed-oop putting - it - all - together

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,19 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
-    
+    def __init__ (self,title,page):
+        self.title = title
+        self.page_count = page
+    @property
+    def page_count(self):
+        return self._page_count
+    @page_count.setter
+    def page_count(self,pageCount):
+        if isinstance(pageCount, int):
+            self._page_count = pageCount
+        else:
+            print("page_count must be an integer") 
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
         
+
